edc02249d148f881ddef3c3d450554be63f9644f
tonytran97 <tonytran_97@live.com>
2022-08-24 11:54:07 -0700

    built foundation

---
+++ script.js
@@ -0,0 +1,14 @@
+$('#rock').on('click', function (ev) {
+    ev.preventDefault();
+    console.log('rock');
+})
+
+$('#paper').on('click', function (ev) {
+    ev.preventDefault();
+    console.log('paper');
+})
+
+$('#scissor').on('click', function (ev) {
+    ev.preventDefault();
+    console.log('scissor');
+})
