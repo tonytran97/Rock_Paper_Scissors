c58e8e3c4671a04173782d8615934fc03660239f
tonytran97 <tonytran_97@live.com>
2022-08-24 12:11:57 -0700

    cpu generated move

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
 $('#rock').on('click', function (ev) {
     ev.preventDefault();
-    console.log('rock');
-})
+    console.log(`user selection: rock`);
+    cpuGenerate();
+});
 
 $('#paper').on('click', function (ev) {
     ev.preventDefault();
-    console.log('paper');
-})
+    console.log(`user selection: paper`);
+    cpuGenerate();
+});
 
 $('#scissor').on('click', function (ev) {
     ev.preventDefault();
-    console.log('scissor');
-})
+    console.log(`user selection: scissors`);
+    cpuGenerate();
+});
+
+// array of choices for the cpu to pick
+cpuChoice = ['rock', 'paper', 'scissors'];
+
+// function that generates a random number and selects from the array above
+function cpuGenerate() {
+    cpuSelect = cpuChoice[Math.floor(Math.random() * cpuChoice.length)];
+    console.log(`cpu selection: ${cpuSelect}`);
+}
