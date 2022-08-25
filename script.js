ce9f4447f29cefe55c5c751fff2fad9bb06e4c0a
tonytran97 <tonytran_97@live.com>
2022-08-24 21:14:11 -0700

    compares user vs cpu and logs result

--- script.js
+++ script.js
@@ -1,26 +1,51 @@
-$('#rock').on('click', function (ev) {
-    ev.preventDefault();
-    console.log(`user selection: rock`);
+cpuChoice = '';
+
+userChoice = '';
+
+$('#rock').on('click', () => {
+    const rock = 'rock';
+    userChoice = rock;
+    console.log(userChoice);
     cpuGenerate();
+    game();
 });
 
-$('#paper').on('click', function (ev) {
-    ev.preventDefault();
-    console.log(`user selection: paper`);
+$('#paper').on('click', () => {
+    const paper = 'paper';
+    userChoice = paper;
+    console.log(userChoice);
     cpuGenerate();
+    game();
 });
 
-$('#scissor').on('click', function (ev) {
-    ev.preventDefault();
-    console.log(`user selection: scissors`);
+$('#scissor').on('click', () => {
+    const scissors = 'scissors'
+    userChoice = scissors;
+    console.log(userChoice);
     cpuGenerate();
+    game();
 });
-
-// array of choices for the cpu to pick
-cpuChoice = ['rock', 'paper', 'scissors'];
 
 // function that generates a random number and selects from the array above
 function cpuGenerate() {
-    cpuSelect = cpuChoice[Math.floor(Math.random() * cpuChoice.length)];
-    console.log(`cpu selection: ${cpuSelect}`);
+    // array of choices for the cpu to pick
+    cpuArray = ['rock', 'paper', 'scissors'];
+    cpuSelect = cpuArray[Math.floor(Math.random() * cpuArray.length)];
+    cpuChoice = cpuSelect;
+    console.log(cpuChoice);
+}
+
+// function that compares the user's choice vs cpu's choice and logs points
+function game() {
+    if (userChoice == 'rock' && cpuChoice == 'scissors') {
+        console.log('+1');
+    } else if (userChoice == 'paper' && cpuChoice == 'rock') {
+        console.log('+1');
+    } else if (userChoice == 'scissors' && cpuChoice == 'paper') {
+        console.log('+1');
+    } else if (userChoice === cpuChoice) {
+        console.log('0');
+    } else {
+        console.log('-1');
+    }
 }
