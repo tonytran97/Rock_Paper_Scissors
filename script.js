8b8d9a947a403b4329fb8e36e279074346131311
tonytran97 <tonytran_97@live.com>
2022-08-24 21:44:24 -0700

    points tracked

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 cpuChoice = '';
 
 userChoice = '';
+score = parseInt($('#score').text());
+console.log(score);
 
 $('#rock').on('click', () => {
     const rock = 'rock';
@@ -35,17 +37,28 @@ function cpuGenerate() {
     console.log(cpuChoice);
 }
 
-// function that compares the user's choice vs cpu's choice and logs points
+// function that compares the user's choice vs cpu's choice and tracks points
 function game() {
     if (userChoice == 'rock' && cpuChoice == 'scissors') {
         console.log('+1');
+        win = score + 1;
+        $('#score').text(win);
     } else if (userChoice == 'paper' && cpuChoice == 'rock') {
         console.log('+1');
+        win = score + 1;
+        $('#score').text(win);
     } else if (userChoice == 'scissors' && cpuChoice == 'paper') {
         console.log('+1');
+        win = score + 1;
+        $('#score').text(win);
     } else if (userChoice === cpuChoice) {
         console.log('0');
+        tie = score;
+        $('#score').text(tie);
     } else {
         console.log('-1');
+        lose = score - 1;
+        $('#score').text(lose);
     }
+    score = parseInt($('#score').text());
 }
